fed977715733131e1d2fa0f435244b4c36668faf
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2023-04-08 14:07:12 -0300

    creat ex008, 9.js and 10,11.html

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log('A velocidae do seu carr é ' + vel + "km/h")
+if(vel > 60){//condição simples
+    console.log('Velocidade maxima ultrapassada, você foi MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
